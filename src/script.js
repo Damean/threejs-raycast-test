f513960dcea57051cb16732e840263d40a545f62
Manuel Abarca <abarcaxyz@gmail.com>
2021-08-11 13:56:52 -0600

    update

--- src/script.js
+++ src/script.js
@@ -12,34 +12,26 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-/**
  * Sizes
  */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+ const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
 window.addEventListener('resize', () => 
 {
-  // Update sizes
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
+// Update sizes
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
 
-  // Update Camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
+// Update Camera
+camera.aspect = sizes.width / sizes.height
+camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// Update renderer
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -50,7 +42,9 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 
 camera.position.z = 3
 scene.add(camera)
 
-// Controls
+/**
+ * Controls
+ */
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
@@ -83,3 +77,14 @@ const tick = () =>
 }
 
 tick()
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+
+
